960b644e66143b7b7b54614e45e0652c97c5f8f3
superUserT <rantshothabisomail@gmail.com>
2024-03-18 09:07:17 +0200

    web does bundle

--- components/CreateContactScreen.js
+++ components/CreateContactScreen.js
@@ -2,25 +2,44 @@ import React, { useState } from 'react';
 import { View, TextInput, Button } from 'react-native';
 import * as Contacts from 'expo-contacts';
 
-export default function CreateContactScreen({ navigation }) {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
+export default function CreateContactScreen() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
 
   const createContact = async () => {
-    // Function to create a new contact
+    const contact = {
+      [Contacts.Fields.FirstName]: firstName,
+      [Contacts.Fields.LastName]: lastName,
+      [Contacts.Fields.PhoneNumbers]: [{ label: 'mobile', number: phoneNumber }],
+    };
+
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      await Contacts.addContactAsync(contact);
+      setFirstName('');
+      setLastName('');
+      setPhoneNumber('');
+    }
   };
 
   return (
     <View>
       <TextInput
-        placeholder="Name"
-        value={name}
-        onChangeText={setName}
+        placeholder="First Name"
+        value={firstName}
+        onChangeText={setFirstName}
       />
       <TextInput
-        placeholder="Phone"
-        value={phone}
-        onChangeText={setPhone}
+        placeholder="Last Name"
+        value={lastName}
+        onChangeText={setLastName}
+      />
+      <TextInput
+        placeholder="Phone Number"
+        value={phoneNumber}
+        onChangeText={setPhoneNumber}
+        keyboardType="phone-pad"
       />
       <Button title="Create Contact" onPress={createContact} />
     </View>
